68701bbf0246da347a317eab7e6bd4596521475f
panasee <49427309+panasee@users.noreply.github.com>
2025-09-21 17:24:07 +0800

    Removed financial sub-module, now a separate repository

--- src/PyOmnix/financial/utils/visualize.py
+++
@@ -1,9 +0,0 @@
-from langchain_core.runnables.graph import MermaidDrawMethod
-from langgraph.graph.state import CompiledGraph
-from pathlib import Path
-
-
-def save_graph_as_png(app: CompiledGraph, output_file_path: Path) -> None:
-    png_image = app.get_graph().draw_mermaid_png(draw_method=MermaidDrawMethod.API)
-    with open(output_file_path, "wb") as f:
-        f.write(png_image)
