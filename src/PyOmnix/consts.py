87eee7ecfc2363b2589311a6ae9da5e983ced35c
panasee <dongkai.pan@gmail.com>
2026-02-17 02:16:12 +0800

    fix encoding conflict

--- src/PyOmnix/consts.py
+++ src/PyOmnix/consts.py
@@ -1,4 +1,4 @@
-"""
+﻿"""
 Constants for the PyOmnix project.
 """
 from __future__ import annotations
@@ -10,46 +10,53 @@ from pathlib import Path
 from pyomnix.omnix_logger import get_logger
 
 logger = get_logger(__name__)
+
 # Path
 OMNIX_PATH: Path | None = None
 LOG_FILE_PATH: str | Path | None = None  # folder for log files
 
-SUCCESS_ICON = "✔️"
-ERROR_ICON = "❌"
-WAIT_ICON = "⏳"
+SUCCESS_ICON = "[OK]"
+ERROR_ICON = "[ERR]"
+WAIT_ICON = "[WAIT]"
+
 
 def enable_rich_traceback() -> None:
     """
-    enable rich traceback
+    Enable rich traceback.
     """
     from rich.traceback import install
+
     install(show_locals=True)
+
 
 def set_paths(*, omnix_path: Path | str | None = None) -> None:
     """
-    two ways are provided to set the paths:
-    1. set the paths directly in the function (before other modules are imported)
-    2. set the paths in the environment variables OMNIX_PATH
+    Two ways are provided to set the paths:
+    1. Set the paths directly in the function (before other modules are imported)
+    2. Set the paths in the environment variables OMNIX_PATH
     """
     global OMNIX_PATH, LOG_FILE_PATH
+
     if omnix_path is not None:
         OMNIX_PATH = Path(omnix_path)
         LOG_FILE_PATH = OMNIX_PATH / "logs"
-    else:
-        omnix_path_str: str | None = os.getenv("OMNIX_PATH")
-        if omnix_path_str is None:
-            pylab_path_str: str | None = os.getenv("PYLAB_DB_LOCAL")
-            if pylab_path_str is not None:
-                logger.info("( *・ω・) read from PYLAB_DB_LOCAL:%s", pylab_path_str)
-                OMNIX_PATH = Path(pylab_path_str)
-                LOG_FILE_PATH = OMNIX_PATH / "logs"
-            else:
-                logger.info("•᷄ࡇ•᷅ PYLAB_DB_LOCAL not set")
-                return
-        else:
-            logger.info("( *・ω・) read from OMNIX_PATH:%s", omnix_path_str)
-            OMNIX_PATH = Path(omnix_path_str)
+        return
+
+    omnix_path_str: str | None = os.getenv("OMNIX_PATH")
+    if omnix_path_str is None:
+        pylab_path_str: str | None = os.getenv("PYLAB_DB_LOCAL")
+        if pylab_path_str is not None:
+            logger.info("^_^ read from PYLAB_DB_LOCAL: %s", pylab_path_str)
+            OMNIX_PATH = Path(pylab_path_str)
             LOG_FILE_PATH = OMNIX_PATH / "logs"
+        else:
+            logger.info("o_O PYLAB_DB_LOCAL not set")
+            return
+    else:
+        logger.info("^_^ read from OMNIX_PATH: %s", omnix_path_str)
+        OMNIX_PATH = Path(omnix_path_str)
+        LOG_FILE_PATH = OMNIX_PATH / "logs"
+
 
 if __name__ == "__main__":
     set_paths(omnix_path="test")
